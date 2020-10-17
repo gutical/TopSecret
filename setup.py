5aca001094256750cfcba0994fb36339d17220ec
gutical <gutitocal@gmail.com>
2020-10-17 12:50:53 +0200

    suck commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Guti',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'gutitocal@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['PYGAME'],
+    'scripts': [],
+    'name': 'PROJECT PYGAME'
+}
+
+setup(**config)
+
